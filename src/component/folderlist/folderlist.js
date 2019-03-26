6a01bdfa4c3d1dd74d9a409aa0afa264e2e96c62
David Bolin <david@bolin.at>
2019-03-26 11:29:15 -0700

    finished technical specs for this stage

--- src/component/folderlist/folderlist.js
+++ src/component/folderlist/folderlist.js
@@ -4,9 +4,26 @@ import { Link } from 'react-router-dom'
 export default class FolderList extends Component {
 
   render() {
+    if (this.props.goBack) {
+      const folder = this.props.folders[0];
+
+      return (
+        <>
+          <ul className="folder-list folder-selected">
+            <li key={folder.id}>
+              <Link to={`/folder/${folder.id}`}>
+                Go Back
+              </Link>
+           </li>
+          </ul>
+          <p>
+           {folder.name}
+          </p>
+        </>
+      );
+    }
 
     return (
-      <>
         <ul className="folder-list">
           {this.props.folders.map(folder =>
             <li key={folder.id} className={folder.id === this.props.selected ? 'folder-selected' : ''}>
@@ -14,13 +31,8 @@ export default class FolderList extends Component {
                 {this.props.goBack ? 'Go Back' : folder.name}
               </Link>
             </li> 
- 
-              
-
           )}
-          {this.props.goBack ? <li>folder.name</li> : ''}
         </ul>
-      </>
     );
   }
 }
