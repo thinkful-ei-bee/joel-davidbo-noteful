10d0aab0182fce1418ab78def2c6bdcc184b6b5e
joelcorey <joelcorey@fastmail.com>
2019-03-26 10:55:48 -0700

    note data render, css adjust

--- src/component/Note/Note.js
+++ src/component/Note/Note.js
@@ -6,16 +6,43 @@ export default class Note extends React.Component {
   render() {
 
     const noteId = this.props.match.params.noteId;
-    const folderId = this.props.notes.find(note => note.id === noteId).folderId;
+
+    const note = this.props.notes.find(note => note.id === noteId);
+
+    //const folderId = note.folderId;
+    
+    // id": "cbc787a0-ffaf-11e8-8eb2-f2801f1b9fd1",
+    // "name": "Dogs",
+    // "modified": "2019-01-03T00:00:00.000Z",
+    // "folderId": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
+    // "content":
 
     return (
       <main role="main" className="App">
       <section className="main-layout">
       <div className="left-menu">
-        <FolderList folders={this.props.folders} selected={folderId}/>
+        <FolderList folders={this.props.folders} selected={note.folderId}/>
       </div>
       <div className="right-content">
-        note stuff here?App
+        
+        <ul className="notes-list">
+          
+            <li key={note.id}>
+              <h3>{note.name}</h3>
+              <p>Date modified: {note.modified}</p>
+              <input 
+                className="favorite styled"
+                type="button"
+                value="Delete note" /> 
+            </li>  
+            <li>
+              <p>
+                {note.content}
+              </p>
+            </li>
+          
+        </ul>
+
       </div>
       </section>
       </main>
