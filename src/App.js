002d812c8ba3e229321ec8cc2a995519fc30168b
joelcorey <joelcorey@fastmail.com>
2019-03-26 06:04:22 -0700

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import DummyStore from './store/dummy-store';
 
 import Header from './component/header/header'
 import Layout from './component/layout/layout'
