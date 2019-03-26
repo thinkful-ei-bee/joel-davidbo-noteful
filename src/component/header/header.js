1e6c59cfa758ad82815378028d91c600b5bb8e1a
joelcorey <joelcorey@fastmail.com>
2019-03-26 08:54:20 -0700

    header route link

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 export default class Header extends Component {
   render() {
     return (
       <header>
-        <h1>Noteful</h1>
+        <h1>
+          <Link to={'/'}>
+            Noteful
+          </Link>
+        </h1>
       </header>
     );
   }
