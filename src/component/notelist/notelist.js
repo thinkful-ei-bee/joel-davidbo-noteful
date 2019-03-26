eb876255e08b5f170b5af41e84bc024496cca020
joelcorey <joelcorey@fastmail.com>
2019-03-26 07:00:14 -0700

    adjust css

--- src/component/notelist/notelist.js
+++ src/component/notelist/notelist.js
@@ -24,7 +24,6 @@ export default class NoteList extends Component {
                 class="favorite styled"
                 type="button"
                 value="Delete note" /> 
-              
             </li>  
           )}
         </ul>
